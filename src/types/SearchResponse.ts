702178cbe19bde92d387d730429d700d69f391c6
Syava <syavaopryshko@gmail.com>
2025-02-16 20:47:30 +0200

    uuup

--- src/types/SearchResponse.ts
+++ src/types/SearchResponse.ts
@@ -1,9 +1,16 @@
 // exported type has all fields not required
 
+type SearchResult = {
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+};
+
 type SearchData = {
-    results: Array<object>;
-    total_pages: number;
-    [field: string]: unknown;
-  };
-  
-  export type SearchResponse = Partial<SearchData>;
+  results: SearchResult[];
+  total_pages: number;
+  [field: string]: unknown;
+};
+
+export type SearchResponse = Partial<SearchData>;
